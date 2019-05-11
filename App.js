066aa5fe12c3745afb7a48acded43be00c66d4a2
adi <pranata1adi@gmail.com>
2019-05-11 14:44:43 +0700

    state

--- App.js
+++ App.js
@@ -1,24 +1,39 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text ,TextInput, View} from 'react-native';
 
-class Mobil extends Component {
-  render() {
-    return (
-      <View style={{margin:5}}>
-        <Text>Mobilku {this.props.name}</Text>
-      </View>
-    );
-  }
-}
+
+
+// class Mobil extends Component {
+//   render() {
+//     return (
+//       <View style={{margin:5}}>
+
+//         {/* this is props <Text>Mobilku {this.props.name}</Text> */}
+//       </View>
+//     );
+//   }
+// }
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      name:'',
+
+    }
+  }
+
   render() {
     return (
       <View style={{flex:1, margin: 20}}>
-        <Mobil name="Ferrari"/>
+        {/* this is props <Mobil name="Ferrari"/>
         <Mobil name="Buggati"/>
         <Mobil name="Audi"/>
-        <Mobil name="Lamborgini"/>
+        <Mobil name="Lamborgini"/> */}
+        <TextInput
+          onChangeText={(text) => {this.setState({nama:text})}}
+        />
+        <Text>Jenis Mobil {this.state.nama}</Text>
       </View>
     );
   }
